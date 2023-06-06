86d441d30db1d7051b0b68ded89c7c25bdeb7c05
川田隼輔 <kwt20238120@gmail.com>
2023-06-06 15:46:18 +0900

    update ディレクトリ構成のためテストファイルを作成

---
+++ frontend/src/pages/Main.js
@@ -0,0 +1,13 @@
+import TestTitle from "../component/TestTitle";
+
+const Main = () => {
+  return (
+    <>
+      <div>
+        <TestTitle />
+      </div>
+    </>
+  );
+};
+
+export default Main;
