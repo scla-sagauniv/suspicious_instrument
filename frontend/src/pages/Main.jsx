f3bfb2b786883c6f92900b34d24ee466fb55ea0d
川田隼輔 <kwt20238120@gmail.com>
2023-06-17 01:10:31 +0900

    refactor 不要なimport文の削除

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,4 +1,3 @@
-import TestTitle from "../component/TestTitle";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import TopPage from "./TopPage";
 import ProjectListPage from "./ProjectListPage";
