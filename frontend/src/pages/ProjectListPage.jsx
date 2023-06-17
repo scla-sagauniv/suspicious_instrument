573b045ba049b38a12dbed3c89371eeb4590ce08
川田隼輔 <kwt20238120@gmail.com>
2023-06-17 18:01:30 +0900

    update マテリアルUI

--- frontend/src/pages/ProjectListPage.jsx
+++ frontend/src/pages/ProjectListPage.jsx
@@ -3,18 +3,18 @@ import dummydata from "../dummydata.json";
 import React, { useState, useEffect } from "react";
 import db from "../firebase/config";
 import { collection, getDocs } from "firebase/firestore";
+import { Button } from "@mui/material";
 
 import "../css/ProjectListPage.css";
 const ProjectListPage = () => {
   //プロジェクトを保持する変数
   const [projects, setProjects] = useState(null);
 
-  const getAllProjects = () => {
+  const getAllProjects = async () => {
     const CollectionRef = collection(db, "Database");
     getDocs(CollectionRef).then((querySnapshot) => {
       querySnapshot.docs.forEach((doc) => {
-        setProjects(doc);
-        console.log(doc);
+        console.log(doc.data());
       });
     });
   };
@@ -33,6 +33,9 @@ const ProjectListPage = () => {
     <>
       <h1 className="projectPageTitle">プロジェクト一覧</h1>
       <ProjectList projects={projects} />
+      <div className="projectAddButton">
+        <Button variant="contained">プロジェクト追加</Button>
+      </div>
     </>
   );
 };
