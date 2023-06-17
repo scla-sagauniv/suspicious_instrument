489820e2fe5231dc9470cbaad3d622607fb27b55
show510ta <show510ta@icloud.com>
2023-06-17 22:25:08 +0900

    update モーダルウィンドウのUIのアップデート

--- backend/main.py
+++ backend/main.py
@@ -1,91 +1,51 @@
 import requests
-import firebase
-import string
 from fastapi import FastAPI
 from pydantic import BaseModel
-import json
-
-FIREBASE = firebase.init_database()
-CHATGPT_API_URL = "https://api.openai.com/v1/chat/completions"
-OPENAI_API_KEY = "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"
+from typing import List
 
 app = FastAPI()
-    
-class Task(BaseModel):
-    project_id: int
-    title: str
-    description: str
-    method: str
-    
-class Member(BaseModel):
-    project_id: int
+
+class InputData(BaseModel):
     name: str
-    skills: str
-    method: str
+    description: str
+    label: str
 
-# デバッグ用
-# with open ("./sample/database.json") as jsondata:
-#     database = json.load(jsondata)[0]
+DB: List[InputData] = [
+    InputData(name="Task01", description="Teach junior about programming", label="Teach"),
+    InputData(name="Task02", description="Learn about programming", label="Learn"),
+]
 
-# Taskの変更を処理する
-@app.post("/task")
-def get_task_data(data: Task):
-    # Firebase からデータベースの取得
-    database = firebase.get_database(FIREBASE)
-    project = int(data.project_id) - 1
-    
-    # キーワードの取得
-    keywords = list()
-    for member in database[project]["members"]:
-        for skill in member["skills"].split(", "):
-            if skill not in keywords:
-                keywords.append(skill)
-    keywords = str(keywords).replace("[", "").replace("]", "").replace("'", "")
-    message = "Select a keyword for '" + str(data.description) + "' from: " + keywords
-    
-    # # ChatGPT処理
+CHATGPT_API_URL = "https://api.openai.com/v1/chat/completions"
+OPENAI_API_KEY = ""
+
+def send_message_to_chatgpt(message):
     headers = {
         "Authorization": f"Bearer {OPENAI_API_KEY}",
         "Content-Type": "application/json",
     }
     payload = {
         "model": "gpt-3.5-turbo",
-        "messages": [{"role": "system", "content": "Answer in only 1 word"}, {"role": "user", "content": message}],
-    }    
+        "messages": [{"role": "system", "content": "You are a helpful assistant."}, {"role": "user", "content": message}],
+    }
     response = requests.post(CHATGPT_API_URL, headers=headers, json=payload)
-    response = response.json()["choices"]
-    available_member = {}
-    if response:
-        prediction = str(response[0]["message"]["content"]).translate(str.maketrans('', '', string.punctuation))
-        
-        for member in database[project]["members"]:
-            if str(prediction) in member["skills"]:
-                available_member[member["id"]] = member["task_count"]
-        available_member = sorted(available_member, reverse=True)
-        database[project]["members"][int(available_member[0]) - 1]["task_count"] += 1
-    else:
-        prediction = "Not applicable"
-    
-    # データベースへの書き込み
-    id = int(len(database[project]["tasks"])) + 1
-    database[project]["tasks"].append({'id': id, 'title': data.title, 'description': data.description, 'assign_member': int(available_member[0])})
-    firebase.write_database(FIREBASE, database)
-    
-    return {"description": data.description, "prediction": str(database[0]["tasks"]), "assign_member": int(available_member[0])}
+    return response.json()
 
-# Memberの変更を処理する
-@app.post("/member")
-def get_member_data(data: Member):
-    # Firebase からデータベースの取得
-    database = firebase.get_database(FIREBASE)
-    project = int(data.project_id) - 1
-    
-    # データベースへの書き込み
-    id = int(len(database[project]["members"])) + 1
-    database[project]["members"].append({'id': id, 'name': data.name, 'skills': data.skills, 'task_count': 0})
-    # firebase.write_database(FIREBASE, database)
+@app.post("/data")
+def receive_data(data: InputData):
+    received_data = data.name
+    received_description = data.description
+    DB.append(InputData(name=received_data, description=received_description, label=""))
 
-# Databaseの確認
-@app.get("/database")
-def get_cloud_database():
-    return firebase.get_database(FIREBASE)
+    chatgpt_response = send_message_to_chatgpt(received_description)
+    # choices = chatgpt_response["choices"]
+    # if choices:
+    #     label = choices[0]["message"]["content"]
+    #     data.label = label
+    # else:
+    #     data.label = "No response from ChatGPT"
+    
+    return {"data": received_data, "description": received_description, "label": chatgpt_response}
+
+@app.get("/")
+def read_root():
+    return DB
--- frontend/src/component/TestTitle.js
+++ frontend/src/component/TestTitle.js
@@ -3,32 +3,22 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 function App() {
-  const [inputId, setInputId] = useState("");
-  const [inputTitle, setInputTitle] = useState("");
+  const [inputName, setInputName] = useState("");
   const [inputDescription, setInputDescription] = useState("");
-  const [inputMethod, setInputMethod] = useState("");
 
-  const handleIdChange = (event) => {
-    setInputId(event.target.value);
-  };
-
-  const handleTitleChange = (event) => {
-    setInputTitle(event.target.value);
+  const handleNameChange = (event) => {
+    setInputName(event.target.value);
   };
 
   const handleDescriptionChange = (event) => {
     setInputDescription(event.target.value);
   };
 
-  const handleMethodChange = (event) => {
-    setInputMethod(event.target.value);
-  };
-
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     try {
-      const response = await axios.post("/task", { project_id: inputId, title: inputTitle, description: inputDescription, method: inputMethod });
+      const response = await axios.post("/data", { name: inputName, description: inputDescription, label: "" });
       console.log(response.data);
     } catch (error) {
       console.error("Error:", error);
@@ -38,18 +28,8 @@ function App() {
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <div>
-          <input type="text" value={inputId} onChange={handleIdChange} />
-        </div>
-        <div>
-        <input type="text" value={inputTitle} onChange={handleTitleChange} />
-        </div>
-        <div>
+        <input type="text" value={inputName} onChange={handleNameChange} />
         <input type="text" value={inputDescription} onChange={handleDescriptionChange} />
-        </div>
-        <div>
-        <input type="text" value={inputMethod} onChange={handleMethodChange} />
-        </div>
         <button type="submit">Submit</button>
       </form>
     </div>
